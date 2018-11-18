a2896cf3363d5fa73e28f821b468acd7f8480d0e
amit20-meet <amit20@meet.mit.edu>
2018-11-18 19:07:42 +0200

    lab6.py

---
+++ lab6.py
@@ -0,0 +1,20 @@
+'''class Turtle(object):
+	"""docstring for Turtle"""
+	def __init__(self, size,shape):
+		super(Turtle, self).__init__()
+		self.size= size
+		Turtle'''
+
+
+
+
+
+
+class Hexagone(object):
+	def __init__(self, size, shape, color):
+		self.size = size
+		self.shape = shape
+		self.color = color
+		
+
+		
